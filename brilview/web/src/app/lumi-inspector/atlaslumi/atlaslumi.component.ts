e761fbb01e546c7b68130769a665e629e9a19f65
DiamantisPa <diamantispa@gmail.com>
2023-07-12 15:33:00 +0200

    update atlas form

--- brilview/web/src/app/lumi-inspector/atlaslumi/atlaslumi.component.ts
+++ brilview/web/src/app/lumi-inspector/atlaslumi/atlaslumi.component.ts
@@ -70,7 +70,6 @@ export class AtlaslumiComponent implements OnInit, AfterViewInit, OnDestroy {
     }
 
     queryBrilLumi(event) {
-        console.log('event bril ', event);
         if (event['fillnum'] != this.fillnum && event['fillnum']) {
             this.onQueryError('Fillnum for the CMS lumi is different than the queried ATLAS lumi');
             return;
