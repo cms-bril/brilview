b0435a6c1f1557479f999ae2c30af3016bbeeb9f
Daniel Košč <daniel.kosc@gmail.com>
2024-09-18 14:24:48 +0200

    Feat: Merged client-compiler and server. Docker image while building clone git repo just once then build Angular app (1st stage) and start server (2nd stage).

--- openshift/brilview/containerfiles/start.sh
+++ openshift/brilview/containerfiles/start.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+# Update brilview Angular app
+rm -rf /client_files_for_serving/*
+cp -R dist/* /client_files_for_serving/
+# Run brilview server
 telegraf &
 /opt/brilconda/bin/brilview --config /brilview_tmp/brilview_openshift_prod.yaml
